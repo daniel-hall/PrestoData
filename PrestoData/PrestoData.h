c982f6a371cf63d2b0693159938047944bc0abf1
Daniel Hall <danhall@outlook.com>
2015-02-13 10:28:35 -0700

    Project file

---
+++ PrestoData/PrestoData.h
@@ -0,0 +1,13 @@
+//
+//  PrestoData.h
+//  PrestoData
+//
+//  Created by Daniel Hall on 2/13/15.
+//  Copyright (c) 2015 Daniel Hall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrestoData : NSObject
+
+@end
---
+++ PrestoData/PrestoData.m
@@ -0,0 +1,13 @@
+//
+//  PrestoData.m
+//  PrestoData
+//
+//  Created by Daniel Hall on 2/13/15.
+//  Copyright (c) 2015 Daniel Hall. All rights reserved.
+//
+
+#import "PrestoData.h"
+
+@implementation PrestoData
+
+@end
